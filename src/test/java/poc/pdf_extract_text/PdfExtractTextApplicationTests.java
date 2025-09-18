f5ddb81276527ccee4a0ba5bc99b9d1cad1bfd82
RIOUX-D <dylan.rioux@prestataire.sihm.fr>
2025-09-18 10:18:25 +0200

    initial commit

---
+++ src/test/java/poc/pdf_extract_text/PdfExtractTextApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.pdf_extract_text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfExtractTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
